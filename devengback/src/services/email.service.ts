8ccf688e741f395ebb24380d01dcfec262fa26f7
Umair Khan <umairkhanone0@gmail.com>
2026-05-09 01:51:44 +0500

    add tls in constructor

--- devengback/src/services/email.service.ts
+++ devengback/src/services/email.service.ts
@@ -13,6 +13,9 @@ export class EmailService {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS,
       },
+      tls: {
+        rejectUnauthorized: false
+      },
     });
   }
 
