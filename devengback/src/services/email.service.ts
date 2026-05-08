52e780325f3628458e41c5fb4305a9ac61b9807c
Umair Khan <umairkhanone0@gmail.com>
2026-05-09 02:03:28 +0500

    secure change to true for 465 port

--- devengback/src/services/email.service.ts
+++ devengback/src/services/email.service.ts
@@ -8,7 +8,7 @@ export class EmailService {
     this.transporter = nodemailer.createTransport({
       host: process.env.EMAIL_HOST || 'smtp.gmail.com',
       port: parseInt(process.env.EMAIL_PORT || '587'),
-      secure: false,
+      secure: true,
       auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS,
