48fd27435bdaf410962fc227d490ca7207fe8eb8
Umair Khan <umairkhanone0@gmail.com>
2026-05-09 07:15:46 +0500

    revert chnanges

--- devengback/src/config/session.ts
+++ devengback/src/config/session.ts
@@ -35,8 +35,8 @@ const sessionMiddleware = session({
   saveUninitialized: false,
   cookie: {
     httpOnly: true,
-    secure: process.env.NODE_ENV === 'development',
-      sameSite: 'none',
+    secure: process.env.NODE_ENV === 'production',
+    sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax',
     maxAge: 24 * 60 * 60 * 1000, // 24 hours
   },
 });
